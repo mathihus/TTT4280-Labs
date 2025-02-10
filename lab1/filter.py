61733f14f749dc994ce6d20f75464a4c3dec455a
mahuse03 <mathi.huse@gmail.com>
2025-02-10 14:37:09 +0100

    all plots

--- lab1/filter.py
+++ lab1/filter.py
@@ -8,8 +8,8 @@ ft = np.linspace(2, int(1e6), int(1e7))
 
 Ht = abs(1/(1-(L*C*(2*np.pi*ft)**2)))
 
-plt.plot(ft, 10*np.log(Ht))
-plt.plot(ft, np.zeros(len(ft))-3, "-")
+plt.plot(ft, 10*np.log(Ht), label=r"Teoretisk |$H(f)$|")
+plt.plot(ft, np.zeros(len(ft))-3, "-", label="-3dB knekkfrekvens")
 # plt.xscale("log")
 # plt.show()
 
@@ -24,8 +24,13 @@ with open("Pifilter.csv", "r") as file:
         Hm.append(float(line[1]))        
 
 
-plt.plot(fm, Hm)
+plt.plot(fm, Hm, label=r"Målt |$H(f)$|")
 plt.xscale("log")
 plt.xlabel("Frekvens, [Hz]")
-plt.ylabel("Amplitude, [Hz]")
+plt.ylabel("Amplitude, [dB]")
+plt.xlim(3, 1e3)
+plt.ylim(-100, 70)
+
+plt.title("Amplituderespons av Pi-filter")
+plt.legend()
 plt.show()
--- lab1/plot.py
+++ lab1/plot.py
@@ -2,42 +2,44 @@ from raspi_import import raspi_import
 import matplotlib.pyplot as plt
 import numpy as np
 
-sample_period, data = raspi_import('1kHz.bin')
+sample_period, data = raspi_import('1k.bin') # converting binary file to array with raspi_import.py
 
 data -= (2**12-1)/2 #+16.5 # subtracting offset
 data = data*(3.3/((2**12)-1)) # scaling y-axis to Volt
 
-data_length = data.shape[0]
-data_channels = data.shape[1]
+data_length = data.shape[0] # getting the length of samples
+data_channels = data.shape[1] # getting amount of channels
 
-n = np.arange(data_length)
-time = n*(sample_period) #scaling x-axis to time in seconds
+n = np.arange(data_length) # making a list with same length as data to be the x-axis of plotting later
+time = n*(sample_period) # scaling x-axis to time in seconds
 
 w = np.sin(np.pi*n/ data_length)**2 # hanning window function
 
-fig, ax = plt.subplots(data_channels, 1) # making subplots
+fig, ax = plt.subplots(data_channels, 1) # setting up figure to put subplots of time series
 fig.tight_layout(pad=0.5)
 
 for i in range(data_channels): # Plotting data channel i
     ax[i].plot(time, data[:, i])
-    ax[i].set_title(f"Channel {i+1} ADC output:")
-    ax[i].set_xlim(0, 0.005)
+    ax[i].set_title(f"ADC {i+1}:")
+    ax[i].set_xlim(0, 0.05)
+    ax[i].set_ylim(-2, 2)
+    ax[i].set_ylabel("Amplitude [V]")
+    ax[i].set_xlabel("Tid [s]")
 plt.show()
 
-N = 2**21 # setting FFT length
-
-N = data_length
+N = 2**15 # setting FFT length to closest 2 power of data_length
+# N = data_length
 
 Sdb = np.zeros((N, data_channels)) # making empty array to put fft values into
 
 fs = int(1/sample_period) # samplings frequency
 df = np.linspace(-0.5, 0.5, N)*fs # making scaled frequency axis
 
-fig1, ax1 = plt.subplots(data_channels, 1)
+fig1, ax1 = plt.subplots(data_channels, 1) # setting up figure to put subplots of PDS
 fig1.tight_layout(pad=0.5)
 
-for i in range(data_channels): 
-    # data[:, i] = w*data[:, i] # using hanning window on channel i
+for i in range(data_channels):
+    data[:, i] = w*data[:, i] # using hanning window on channel i
 
     fft = abs(np.fft.fft(data[:, i], N)) # reshaping data to be an 1d array and taking fft
     fft = abs(np.fft.fftshift(fft)) # shifting spectrum and taking absolute value 
@@ -45,27 +47,34 @@ for i in range(data_channels):
     Sdb[:, i] = 20*np.log10(fft/max(fft)) # db scale and normalising of fft
 
     ax1[i].plot(df, Sdb[:, i])
-    ax1[i].set_title(f"Channel {i+1} power density spectrum:")
-    ax1[i].set_xlim(0, )
+    ax1[i].set_title(f"ADC {i+1}:")
+    ax1[i].set_xlim(10, 15000)
+    ax1[i].set_ylim(-120,)
+    ax1[i].set_ylabel("Effekt [dB]")
+    ax1[i].set_xlabel("Frekvens [Hz]")
+    ax1[i].set_xscale("log")
 plt.show()
 
 for i in range(data_channels):
-    f = np.argmax(Sdb[int(N/2):, i])
+    f = np.argmax(Sdb[int(N/2):, i]) # finding the frequency of the signal. Only works with no zero-padding, else enter it manually.
+    f = 1000
     print(f"f = {f}Hz")
-    B = f/10
+    B = f/10 # defining the bandwidth of the desired frequency for SNR calculations
 
     # signal_power = np.mean(Sdb[int((N)*(sample_period*(f-(B/2)) + 1/2)):int((N)*(sample_period*(f+(B/2)) + 1/2)), i])
     signal_power = 0 # because of the normalisation of PDS
-    
+
+    # taking the max value from the PDS from 10 -> f-(B/2) and f+(B/2) -> inf
+    noise_power_harmonic = max(max(Sdb[int(N/2)+10:int((N)*(sample_period*(f-(B/2)) + 1/2)), i]), max(Sdb[int((N)*(sample_period*(f+(B/2)) + 1/2)):, i]))
+
     # taking the max value from the PDS from 10 -> f-(B/2) and f+(B/2) -> 2*f-(B/2). Only looking to 2*f-(B/2) to neglect the harmonic frequencies.
     noise_power = max(max(Sdb[int(N/2)+10:int((N)*(sample_period*(f-(B/2)) + 1/2)), i]), max(Sdb[int((N)*(sample_period*(f+(B/2)) + 1/2)):int((N)*(sample_period*(2*f-(B/2)) + 1/2)), i]))
 
-    print("Signal_rms² =", signal_power)
-    print("Noise_rms² =", noise_power)
-
-    print("SNR", signal_power-noise_power)
+    print("SNR includic harmonic noise", signal_power-noise_power_harmonic)
+    print("SNR excluding harmonic noise", signal_power-noise_power)
 
 print("Theoretical SNR max =", 20*np.log10((np.sqrt(6)/2)*(2**12)*(3.3/3.3)))
 
 
 
+    
