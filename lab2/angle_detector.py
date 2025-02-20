b28919b89b36382c379f4dd5c0cd83bf9ad78228
mahuse03 <mathi.huse@gmail.com>
2025-02-20 19:44:49 +0100

    fiksa alt

--- lab2/angle_detector.py
+++ lab2/angle_detector.py
@@ -3,82 +3,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math as m
 
-deg = 45
-
-sample_period, data = raspi_import(f'data/{deg}/{deg}_deg_4.bin') # converting binary file to array with raspi_import.py
-
-data -= (2**12-1)/2 #+16.5 # subtracting offset
-data = data*(3.3/((2**12)-1)) # scaling y-axis to Volt
-
-skip_samples = 3000
-
-data_length = data.shape[0]-skip_samples # getting the length of samples
-data_channels = 3 # getting amount of channels
-
-signal1, signal2, signal3 = data[skip_samples:, 2], data[skip_samples:, 1], data[skip_samples:, 0]
-
-# max_lag = 6
-# n_lag = 5
-# signal2 = data[skip_samples:-n_lag, 0]
-# signal1 = data[skip_samples+n_lag:, 0]
-# signal3 = data[skip_samples+n_lag:, 0]
-
-# def lag(signal1, signal2):
-#     cross_corr = np.zeros(2*max_lag+1)
-
-#     for i in range(len(cross_corr)):
-#         cross_corr[i] = np.correlate(signal1[i:-len(cross_corr)+i], signal2[:-len(cross_corr)])        
-#         # print(np.correlate(signal1[i:len(signal1)-len(cross_corr)+i], signal2[:(len(signal2)-len(cross_corr))]))
-
-#     return -np.argmax(cross_corr) # returning the lag that gives the highes crosscorrolation
-
-
-n = np.arange(data_length) # making a list with same length as data to be the x-axis of plotting later
-time = n*(sample_period) # scaling x-axis to time in seconds
-
-def lag(signal1, signal2):
+def lag(signal1, signal2, mic):
     corr = np.correlate(signal1, signal2, "full")
 
-    lag_n = data_length
-    x = np.arange(lag_n-10, lag_n+10)
+    lag_n = len(signal1)
+    x = np.arange(lag_n-50, lag_n+50)
     interpcorr = np.interp(x, np.arange(len(corr)), corr)
 
-    # print(interpcorr)
+    lag_estimate = (np.argmax(interpcorr))-len(x)//2+1
 
-    lag_estimate = (np.argmax(interpcorr))-len(x)/2+1
-    # lag_estimate = (np.argmax(corr))-data_length+1
-    # print(lag_estimate)
+    plt.plot(x, interpcorr, label=mic)
+    return -lag_estimate
 
-    plt.plot(x, interpcorr)
-    return lag_estimate
+def angle(signal1, signal2, signal3, plot=False):
+    n21 = lag(signal2, signal1, "n21")
+    n31 = lag(signal3, signal1, "n31")
+    n32 = lag(signal3, signal2, "n32")
 
-# corr_lag = lag(signal1, signal2, max_lag)
-# print(corr_lag, corr_lag*sample_period)
-
-def angle(signal1, signal2, signal3):
-    n21 = lag(signal2, signal1)
-    n31 = lag(signal3, signal1)
-    n32 = lag(signal3, signal2)
-
-    plt.show()
+    if plot:
+        plt.legend()
+        plt.show()
 
     print(n21, n31, n32)
 
-    angle_estimate = np.arctan(np.sqrt(3)*((n31+n21)/(n31-n21+2*n32)))# - (10/180)*np.pi
+    angle_estimate = -np.arctan(np.sqrt(3)*((n31+n21)/(n31-n21+2*n32)))
 
     print(angle_estimate*180/np.pi)
 
     # angle_estimate = np.pi - angle_estimate
 
-    if angle_estimate < 0:
-        angle_estimate += 2*np.pi
-    elif m.copysign(1, angle_estimate) < 0:
+    if (n31-n21+2*n32) < 0:
         angle_estimate += np.pi
+    elif angle_estimate < 0:
+        angle_estimate += 2*np.pi
 
     return angle_estimate
 
-angle_estimate = angle(signal1, signal2, signal3)
-print(angle_estimate*180/np.pi)
+def angle_detect(deg, i, plot=False):
+    sample_period, data = raspi_import(f'data/{deg}/{deg}_deg_{i}.bin') # converting binary file to array with raspi_import.py
+
+    data -= (2**12-1)/2 #+16.5 # subtracting offset
+    data = data*(3.3/((2**12)-1)) # scaling y-axis to Volt
+
+    skip_samples = 3000
+
+    signal1, signal2, signal3 = data[skip_samples:, 2], data[skip_samples:, 0], data[skip_samples:, 1]
+
+    angle_estimate = angle(signal1, signal2, signal3, plot)
+    
+    return abs(angle_estimate*180/np.pi)
+
+for i in range(8):
+    print(45*i, "-", angle_detect(45*i, 2))
 
 # fig, ax = plt.subplots(data_channels, 1) # setting up figure to put subplots of time series
 # fig.tight_layout(pad=0.5)
