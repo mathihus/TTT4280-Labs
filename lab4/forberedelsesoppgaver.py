c08a641f92aa93416692c1e02688461f0a62296a
magnusgogstad <113847104+magnusgogstad@users.noreply.github.com>
2025-03-13 18:09:40 +0100

    lab4 ting

---
+++ lab4/forberedelsesoppgaver.py
@@ -0,0 +1,26 @@
+import numpy as np
+c = 299792458
+
+def dopplerskift(v_rad):
+    f_d = (2*(24.13*10**9)*v_rad)/c
+    return f_d
+
+def måleoppløsning(T):
+    dirac_v = 1/T
+    return dirac_v
+
+def antennevinning(deg_azi, deg_elev):
+    G = 10*np.log10((30000/(deg_azi*deg_elev)))
+    return G
+
+def sigma(a, f_0):
+    bølgelengde  = c/(f_0)
+    sigma = (4*np.pi*((a)**4))/(4*(bølgelengde**2))
+    return sigma
+
+print("foreberedelse 2.1.1: ",dopplerskift(25))
+print("foreberedelse 2.1.2: ",måleoppløsning(1))
+print("foreberedelse 2.1.3: ",antennevinning(12, 80))
+print("foreberedelse 2.1.4: ",sigma(0.155, (24*(10**9))))
+
+
