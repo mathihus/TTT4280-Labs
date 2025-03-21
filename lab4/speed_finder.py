132644ab234d23143bc676300f6a9e068e934d1a
mahuse03 <mathi.huse@gmail.com>
2025-03-21 12:41:42 +0100

    succesful dopler detection

--- lab4/speed_finder.py
+++ lab4/speed_finder.py
@@ -2,13 +2,15 @@ from raspi_import import raspi_import
 import matplotlib.pyplot as plt
 import numpy as np
 
-sample_period, data = raspi_import('data/rygg3.bin') # converting binary file to array with raspi_import.py
+sample_period, data = raspi_import('data/speed2.bin') # converting binary file to array with raspi_import.py
 
 data -= (2**12-1)/2 #+16.5 # subtracting offset
 data = data*(3.3/((2**12)-1)) # scaling y-axis to Volt
 
-sample_start = data.shape[0]//10
-sample_stop = data.shape[0]//4
+second_interval = 5 # only 1-9 are valid inputs
+
+sample_start = data.shape[0]//(11-second_interval)
+sample_stop = data.shape[0]//(10-second_interval)
 
 Q, I = data[sample_start:sample_stop, 3], data[sample_start:sample_stop, 4]
 
@@ -33,29 +35,31 @@ def v_rad(f_d):
     v = (c*f_d)/(2*f_0)
     return v
 
+def plotter(time, frequency, signal, spectrum):
+
+    plt.plot(time, np.real(signal))
+    plt.plot(time, np.imag(signal))
+    plt.show()
+
+    plt.plot(frequency, spectrum)
+    plt.xlim(-1000, 1000)
+    plt.ylim(-50, 5)
+    plt.show()
+
 N = 2**18 # setting FFT length to closest 2 power of data_length
 # N = data_length
 
 fs = int(1/sample_period) # samplings frequency
 df = np.linspace(-0.5, 0.5, N)*fs # making scaled frequency axis
 
-plt.plot(time, I)
-plt.plot(time, Q)
-plt.show()
-
 dopler = I+(1j*Q)
-
 # dopler = dopler*w
+dopler_spectrum = fft(dopler, N)
 
-dopler = fft(dopler, N)
+plotter(time, df, dopler, dopler_spectrum)
 
-plt.plot(df, dopler)
-plt.xlim(-1000, 1000)
-plt.ylim(-50, 5)
-plt.show()
-
-f_d = -320
+f_d = df[np.argmax(dopler_spectrum)]
+print(f_d, "Hz")
 speed = v_rad(f_d)
-
 print(speed, "m/s")
-print(speed*3.6, "km/h")
+# print(speed*3.6, "km/h")
